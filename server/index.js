93e1b3965a697bdddb543dba3b5f3c1efe1149a2
nadzeyaharbuzava <nadzeyakuchko@gmail.com>
2026-02-20 23:14:27 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+
+
+const app = express()
+
+app.use(cors({origin: 'http://localhost:5173'}))
+app.use(express.json())
+
+//health check
+
+app.get('/',(req,res)=>{
+    res.json({message: 'ApplyWise is running 🚀'})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=> console.log(`Server running on port ${PORT}`))
