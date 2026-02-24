b79f3caed68a9bf621ef87e5f1762c5476d41c45
unknown <nadzeyakuchko@gmail.com>
2026-02-24 23:52:39 +0200

    chore: add production env config

--- server/index.js
+++ server/index.js
@@ -12,7 +12,22 @@ const errorHandler=require('./middleware/errorHandler')
 
 const app = express()
 
-app.use(cors({origin: 'http://localhost:5173'}))
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL  
+]
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
+
 app.use(express.json())
 
 //routes
